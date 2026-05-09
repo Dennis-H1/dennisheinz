8da1c11f596740b87559e01e261ff088f5d8f21e
dennis_heinz <dennis.heinz2604@gmail.com>
2026-05-09 16:32:07 +0200

    fix trailing slash redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://dennisheinz.com",
+  trailingSlash: "always",
   integrations: [sitemap()],
   build: {
     inlineStylesheets: "always",
+    format: "directory",
   },
 });
