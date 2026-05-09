17fe196eec62c467648a65f27be4cda3204aab0d
dennis_heinz <dennis.heinz2604@gmail.com>
2026-05-09 16:03:01 +0200

    fix datenschutz

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,5 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://dennisheinz.com",
-  i18n: {
-    defaultLocale: "de",
-    locales: ["de", "en"],
-    routing: {
-      prefixDefaultLocale: false,
-    },
-  },
   integrations: [react(), sitemap()],
 });
