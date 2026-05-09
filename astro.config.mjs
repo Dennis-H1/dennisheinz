60af0117f5ced0de89c21fb5a1e4791654f6b9d5
dennis_heinz <dennis.heinz2604@gmail.com>
2026-05-09 16:23:39 +0200

    improve performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://dennisheinz.com",
-  integrations: [react(), sitemap()],
+  integrations: [sitemap()],
+  build: {
+    inlineStylesheets: "always",
+  },
 });
